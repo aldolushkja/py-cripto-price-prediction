5839359386d01031430c2274eb01cf70dcd8cff1
aldolushkja <aldo.lushkja@gmail.com>
2021-06-29 16:28:14 +0200

    testing the model pt2

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ model.add(LSTM(units=50, return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(units=50))
 model.add(Dropout(0.2))
-model.Dense(units=1)
+model.add(Dense(units=1))
 
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
@@ -65,3 +65,19 @@ x_test = []
 
 for x in range(prediction_days, len(model_inputs)):
     x_test.append(model_inputs[x - prediction_days:x, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+prediction_prices = model.predict(x_test)
+prediction_prices = scaler.inverse_transform(prediction_prices)
+
+plt.plot(actual_prices, color='black', label='Actual Prices')
+plt.plot(prediction_prices, color='green', label='Predicted Prices')
+plt.title(f'{crypto_currency} price prediction')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend(loc='upper left')
+plt.show()
+
+# Predict Next Day
