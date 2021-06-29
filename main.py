989d3b7d1363cbeb3854e3e525832f4fec2b08b3
aldolushkja <aldo.lushkja@gmail.com>
2021-06-29 16:30:33 +0200

    price prediction

--- main.py
+++ main.py
@@ -81,3 +81,11 @@ plt.legend(loc='upper left')
 plt.show()
 
 # Predict Next Day
+
+real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs) + 1, 0]]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+print(prediction)
